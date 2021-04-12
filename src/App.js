e876065a6bce278d70a64beffe5288bf999c4690
Tuyen Pham <tuh39709@temple.edu>
2021-04-12 11:45:29 -0400

    spotify login

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react'
 import './App.css';
+import Login from './components/Login/Login'
 
 function App() {
   return (
-    <div className="App">
-      Spotify clone
+    <div className="app">
+
+      {/* Spotify logo */}
+      {/* Login with spotify button */}
+      <Login />
+
     </div>
   );
 }
